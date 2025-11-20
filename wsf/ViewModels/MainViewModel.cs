f1ff3717003a94da69fd5a6bc3455403e2141d36
77f315 <77f315@gmail.com>
2025-11-20 05:38:58 +0200

    can disable bing

---
+++ wsf/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wsf.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private const string RegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Search";
+        private const string BingKey = "BingSearchEnabled";
+        private const string CortanaKey = "CortanaConsent";
+
+        private bool _isBingEnabled;
+
+        public bool IsBingEnabled
+        {
+            get => _isBingEnabled;
+            set
+            {
+                if (_isBingEnabled != value)
+                {
+                    _isBingEnabled = value;
+                    OnPropertyChanged(nameof(IsBingEnabled));
+                    SetBingState(value);
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            _isBingEnabled = GetBingState();
+        }
+
+        private bool GetBingState()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryPath))
+            {
+                if (key == null) return true; 
+                object bingValue = key.GetValue(BingKey, 1);
+                return bingValue != null && (int)bingValue != 0;
+            }
+        }
+
+        private void SetBingState(bool enabled)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(RegistryPath, true))
+            {
+                if (key == null) return;
+                key.SetValue(BingKey, enabled ? 1 : 0, RegistryValueKind.DWord);
+                key.SetValue(CortanaKey, enabled ? 1 : 0, RegistryValueKind.DWord);
+            }
+        }  
+    }
+}
